da0505004e65d293931fd0125a7b64b388e12984
Shishikyuu <richard.febres@gmail.com>
2021-02-11 20:12:14 -0500

    Add tests in CalculatorTests.cs

--- CalculatorProjectTests/CalculatorTests.cs
+++ CalculatorProjectTests/CalculatorTests.cs
@@ -24,15 +24,56 @@ namespace CalculatorProject.Tests
         {
             //arrange
             double _a = 1;
-            double _b =2;
+            double _b = 2;
             double _c;
-            
+
             //act
             _c = Addition.Sum(_a, _b);
 
             //Assert
             Assert.AreEqual(_c, 3);
         }
+
+        [TestMethod()]
+        public void MinusTest()
+        {
+            double _a = 5.0;
+            double _b = 1.0;
+            double _c;
+
+            _c = Subtraction.Minus(_a, _b);
+
+            Assert.AreEqual(_c, 4.0);
+        }
+
+
+        [TestMethod()]
+        public void TimesTest()
+        {
+            double _a = 4.0;
+            double _b = 3.0;
+            double _c;
+
+            _c = Multiplication.Times(_a, _b);
+
+            Assert.AreEqual(_c, 12.0);
+
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            double _a = 10.0;
+            double _b = 2.0;
+
+            double _c;
+
+            _c = Division.Divide(_a, _b);
+
+            Assert.AreEqual(_c, 5.0);
+        }
+            
+   
         [TestMethod()]
         public void CalculationTest()
         {
