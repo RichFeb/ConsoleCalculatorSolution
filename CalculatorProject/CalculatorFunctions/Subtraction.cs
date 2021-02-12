7547fa176b8cc20fe7354e335f41680b3b155282
Shishikyuu <richard.febres@gmail.com>
2021-02-11 20:11:40 -0500

    Add Subtraction class

---
+++ CalculatorProject/CalculatorFunctions/Subtraction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorProject.CalculatorFunctions
+{
+    public class Subtraction
+    {
+        public static double Minus(double a, double b)
+        {
+            double _c;
+            _c = a - b;
+            return _c;
+        }
+
+    }
+}
