65d5c94f3fd6ba71a998a12a84b122a8707c19b0
Shishikyuu <richard.febres@gmail.com>
2021-02-11 20:11:23 -0500

    Add Division class

---
+++ CalculatorProject/CalculatorFunctions/Division.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorProject.CalculatorFunctions
+{
+    public class Division
+    {
+        public static double Divide(double _a, double _b)
+        {
+            double _c;
+            _c = _a / _b;
+            return _c;
+        }
+    }
+}
